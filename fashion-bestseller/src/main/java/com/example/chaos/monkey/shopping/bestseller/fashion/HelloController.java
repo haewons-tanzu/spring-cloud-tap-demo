5d09d19ef824594d0a16afac3742a60de1805600
haewons-tanzu <haewons@vmware.com>
2023-06-25 20:22:54 +0900

    commit

--- fashion-bestseller/src/main/java/com/example/chaos/monkey/shopping/bestseller/fashion/HelloController.java
+++ fashion-bestseller/src/main/java/com/example/chaos/monkey/shopping/bestseller/fashion/HelloController.java
@@ -1,14 +1,20 @@
 package com.example.chaos.monkey.shopping.bestseller.fashion;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class HelloController {
+	
+	@Value("${cook.special:none}")
+	String special;
 
 	@RequestMapping("/")
 	public String index() {
-		String str = "Greetings from Spring Boot + Tanzu!!";
+		String str = "Greetings from Spring Boot + Tanzu!!" + special;
 		return str;
 	}
 
