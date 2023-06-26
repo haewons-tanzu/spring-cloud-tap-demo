498137b17563a6f2080ce163d8c2574bca3def48
haewons-tanzu <haewons@vmware.com>
2023-06-26 14:22:14 +0900

    commit

--- fashion-bestseller/src/main/java/com/example/chaos/monkey/shopping/bestseller/fashion/BestsellerFashionApplication.java
+++ fashion-bestseller/src/main/java/com/example/chaos/monkey/shopping/bestseller/fashion/BestsellerFashionApplication.java
@@ -2,11 +2,10 @@ package com.example.chaos.monkey.shopping.bestseller.fashion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
-///thisdfvdfasd
+@EnableDiscoveryClient
 public class BestsellerFashionApplication {
 
 	public static void main(String[] args) {
